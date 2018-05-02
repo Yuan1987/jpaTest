47582c5fa60b11019cc9bab145757ec97f02b366
yuanhangbiao <yuanhangbiao@LIANGDONG.yulintu.local>
2018-05-02 14:07:59 +0800

    加@entity

--- src/main/java/com/jpatest/modules/models/Test.java
+++ src/main/java/com/jpatest/modules/models/Test.java
@@ -1,13 +1,17 @@
 package com.jpatest.modules.models;
 
 import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * Test is a Querydsl bean type
  */
 @Generated("com.querydsl.codegen.BeanSerializer")
+@Entity
 public class Test {
 
+	@Id
     private Integer id;
 
     private String name;
@@ -28,10 +32,15 @@ public class Test {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-         return "id = " + id + ", name = " + name;
-    }
-
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Test [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
 }
 
