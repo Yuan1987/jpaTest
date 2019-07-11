deeb4730b7cdc9c0e8ee2f4818716b64bd224647
yuan <yuan@DESKTOP-8UVDU0U>
2019-07-11 10:15:10 +0800

    shell

--- shell.sh
+++ shell.sh
@@ -1,14 +1,12 @@
-#docker 镜像/容器名字或者jar名字 这里都命名为这个
+#!/bin/sh
+#docker 镜像/容器名字
 SERVER_NAME=jpa-test
 BASE_PATH=docker/
-
 cp target/JpaTest-0.1.jar $BASE_PATH
-
 #容器id
 CID=$(docker ps -a | grep "$SERVER_NAME" | awk '{print $1}')
 #镜像id
 IID=$(docker images | grep "$SERVER_NAME" | awk '{print $3}')
-
 # 删除docker容器
 if [ -n "$CID" ]; then
         echo "存在$SERVER_NAME容器，CID=$CID"
@@ -25,10 +23,8 @@ if [ -n "$IID" ]; then
 else
         echo "不存在已废弃镜像"
 fi
-
 cd $BASE_PATH
 docker build -t  $SERVER_NAME .
-
 # 运行docker容器
 docker run --name $SERVER_NAME -e SPRING_PROFILES_ACTIVE="dev" -d -p 8081:8081 $SERVER_NAME
 echo "$SERVER_NAME容器创建完成"
